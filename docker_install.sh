02908af93953db79bc749305601b2e17960ff7fd
Aliao <omz1990518@gmail.com>
2021-01-19 21:35:39 +0800

    upgrade docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -6,6 +6,7 @@
 # @Last Modified time: 2021-01-18 23:30:08
 
 A=$(uname -r | cut -d "." -f1-2)
+SCRIPT="docker_install.sh"
 
 #检验内核版本
 CheckVersion(){
@@ -31,4 +32,94 @@ DockerInstall(){
     systemctl start docker    
 }
 
+#启动docker
+STARTDocker(){
+    systemctl start docker
+}
 
+#停止docker
+STOPDocker(){
+    systemctl stop docker
+}
+
+#重启docker
+RELOADDocker(){
+    systemctl reload docker
+}
+
+#测试doucker
+TESTDocker(){
+    docker run hello-world
+}
+
+#卸载docker
+REMOVEDocker(){
+    yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate  docker-engine
+    echo "----------------------------------卸载docker完成"
+    find / -name $SCRIPT -exec rm -f {} \;
+}
+
+
+#脚本菜单
+STARTMenu(){
+    clear
+    echo "========================================================================="
+    echo "#                                                                       #"
+    echo "#                  @Name: docker_oneclick_install_script                #"
+    echo "#                  @Author: Aliao                                       #"
+    echo "#                  @Repository: https://github.com/vod-ka               #"
+    echo "#                                                                       #"
+    echo "========================================================================="
+    echo "                          "
+    echo "                          "
+    echo "                          "
+    echo "1，安装docker"
+    echo "--------------------------"
+    echo "2，启动docker"
+    echo "--------------------------"
+    echo "3，停止docker"
+    echo "--------------------------"
+    echo "4，重启docker"
+    echo "--------------------------"
+    echo "5，检测docker"
+    echo "--------------------------"
+    echo "6，卸载docker"
+    echo "--------------------------"
+    echo "0，exit"
+    echo "--------------------------"
+    read -p "输入数字执行" MUN
+    case "$MUN" in
+        1)
+        CheckVersion
+        STARTMenu
+        ;;
+        2)
+        STARTDocker
+        clear
+        STARTMenu
+        ;;
+        3)
+        STOPDocker
+        clear
+        STARTMenu
+        ;;
+        4)
+        RELOADDocker
+        clear
+        STARTMenu
+        ;;
+        5)
+        TESTDocker
+        clear
+        STARTMenu
+        ;;
+        6)
+        REMOVEDocker
+        ;;
+        0)
+        exit 1
+        ;;
+    esac
+}
+
+STARTMenu
