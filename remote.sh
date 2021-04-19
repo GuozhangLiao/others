4b797e66f97f90e3e950c01d9b7301d6de6570ec
Aliao <omz1990518@gmail.com>
2021-04-19 16:11:35 +0800

    添加网络检测

--- remote.sh
+++ remote.sh
@@ -12,6 +12,10 @@ Green(){
     echo -e "\033[32;01m$1\033[0m"
 }
 
+Red(){
+    echo -e "\033[31;01m$1\033[0m"
+}
+
 title(){
     clear
     Green "#===============================================================================#"
@@ -28,7 +32,7 @@ title(){
     read -rp "输入数字执行: " Num
     case $Num in 
         1)
-        ation
+        Ation
         ;;
         2)
         exit 0
@@ -36,26 +40,35 @@ title(){
     esac
 }
 
-ation(){
+Ation(){
     clear
     read -rp "请输入远程服务器的IP地址： " -t 60 a
     clear
-    Green "1,用户名默认为administrator\n------------"
-    Green "2,手动输入\n----------"
-    read -rp "请输入数字执行： " shuzi
-    case $shuzi in
-        1)
-        b="administrator"
-        ;;
-        2)
-        read -rp "请输入用户名： " -t 60 b
-        ;;
-    esac
-    clear
-    read -rp "请输入密码 " -t 60 c
-    clear
-    Green "rdesktop远程链接中，如需断开链接请按Ctrl+C!"
-    rdesktop -u "$b" -p "$c" "$a" > /dev/null 2>&1
+    ping -c 1 "$a" > /dev/null 2>&1
+    local p=$?
+    if [ $p -eq 0 ]
+    then
+        clear
+        Green "1,用户名默认为administrator\n------------"
+        Green "2,手动输入\n----------"
+        read -rp "请输入数字执行： " shuzi
+        case $shuzi in
+            1)
+            b="administrator"
+            ;;
+            2)
+            read -rp "请输入用户名： " -t 60 b
+            ;;
+        esac
+        clear
+        read -rp "请输入密码 " -t 60 c
+        clear
+        Green "rdesktop远程链接中，如需断开链接请按Ctrl+C!"
+        rdesktop -u "$b" -p "$c" "$a" > /dev/null 2>&1
+    else
+        Red "指定地址无法连接，请检查地址！"
+        exit 0
+    fi
 }
 
 #main
