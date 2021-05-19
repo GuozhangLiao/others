1272db50c28ea61724c12f3e1b77e95f19fe3926
Aliao <omz1990518@gmail.com>
2021-05-19 13:16:48 +0800

    简单修改

--- 删歌脚本/create.sh
+++ 删歌脚本/create.sh
@@ -6,5 +6,10 @@
 # @Last Modified time: 2021-04-21 20:14:36  
 for line in $(cat a.txt)
 do
-    touch "$line".{txt,mp4,3gp,evb}
+    #touch "$line".{txt,mp4,3gp,evb}
+    find /mnt/* -name "$line".mp4 -exec mv {} /home/music/ \;
+    find /mnt/* -name "$line".3gp -exec mv {} /home/music/ \;
+    find /mnt/* -name "$line".evb -exec mv {} /home/music/ \;
+    find /mnt/* -name "$line".mkv -exec mv {} /home/music/ \;
+    find /mnt/* -name "$line".mov -exec mv {} /home/music/ \;
 done
